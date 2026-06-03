bd9ec9bc15cca6b1c257111895c5550700e49100
omgutty <om.gutty@gmail.com>
2026-06-03 10:41:20 +0530

    <develop> data generated utility, and wait helper utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,13 @@
+
+
+/**
+ * Normal exports (export { ... }) become JavaScript imports/exports and exist at runtime.
+Type exports (export type { ... }) are removed completely during compilation because types are only needed during development and type checking.
+ */
 export { Logger, LogLevel } from './Logger';
 
 // why we have created index.ts in utils folder and exportedfrom here ?
-
+export { WaitHelper } from './WaitHelper';
 export { ApiHelper } from './ApiHelper';
+export {DataGenerator} from './DataGenerator';
 export type { ApiRequestOptions, RetryOptions, HttpMethod } from './ApiHelper';
